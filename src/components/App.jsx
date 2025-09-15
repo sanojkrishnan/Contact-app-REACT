d954c5ed109a31b7ca517abac27199cc003eee91
sanojkrishnan <sanojkrishnan12@gmail.com>
2025-09-15 21:57:14 +0530

    date 15/09/2025

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import ContactList from "./ContactList"; //ContactList component import
 
 function App() {
   const [ contacts, setContacts ] = useState([]);
+  const [loaded, setLoaded] = useState(false);  //to prevent overwriting of contacts when component re-renders.
 
   const LOCAL_STORAGE_KEY = "contacts"; //key to store the contacts in local storage.
 
@@ -18,21 +19,24 @@ function App() {
      setContacts(prevContacts => [{ id: Date.now(), ...contact }, ...prevContacts]); //adding new contact to the contacts along with a unique id using Date.now()
     console.log("contacts in App.jsx", contacts);
   };
+
      //---------------------lifecycle methods for adding contacts---------------------------//
-   useEffect(()=>{     //useEffect is to control the lifecycle methods of the contacts array.
-    const storedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));  //here we are getting the contacts array from the local storage.
-    if(storedContacts){   //this will only run if there are contacts in the local storage
-        setContacts(storedContacts); //setting the contacts array to the state.
+
+  useEffect(()=>{ 
+    if(loaded) {  
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))   //here we are using local storage to store the contacts array in the browser.
     }
-  },[]) //empty dependency array means this will only run once when the component is mounted.
+  },[contacts, loaded]); //this will run every time the contacts array changes or when the component is loaded.
 
 
   useEffect(()=>{     //useEffect is to control the lifecycle methods of the contacts array.
-    if (contacts.length > 0) {
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))   //here we are using local storage to store the contacts array in the browser.
+    const storedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));  //here we are getting the contacts array from the local storage.
+    if(storedContacts){   //this will only run if there are contacts in the local storage
+        setContacts(storedContacts); //setting the contacts array to the state.
 
+         setLoaded(true); //setting loaded to true so that the contacts are only loaded once when the component is mounted.
     }
-  },[contacts])
+  },[]) //empty dependency array means this will only run once when the component is mounted.
 
 
   //---------------------Removing contacts---------------------------//
@@ -40,7 +44,6 @@ function App() {
   const removeContactHandler = (id) => {
     setContacts(contacts.filter((item) =>item.id !==id )); //filtering not the contact which is to be removed.
     console.log("contact removed", id);
-
   }
 
   return (
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -5,21 +5,36 @@ function ContactList(props) {
   //props is used to get the data from parent component, here we are getting "contacts" array from App.jsx(parent component).
   console.log(props);
 
-  const {contacts, getContactId} = props;
+  const { contacts, getContactId } = props;
 
   const deletionHandler = (id) => {
     getContactId(id);
-  }
+  };
 
   const renderContactList = contacts.map((contacts) => {
     console.log("item to contact card", contacts);
     return (
       <>
-      <ContactCard item={contacts} clickHandler={deletionHandler} key={contacts.id} />
+        <ContactCard
+          item={contacts}
+          clickHandler={deletionHandler}
+          key={contacts.id}
+        />
       </>
     );
   });
-  return <div className="ui celled list">{renderContactList}</div>;
+  return (
+    <>
+      <div className="ui name">
+        <br/>
+        <br/>
+        <h2>Contact List</h2>
+        <br/>
+        <br/>
+      </div>
+      <div className="ui celled list">{renderContactList}</div>
+    </>
+  );
 }
 
 export default ContactList;
